4e63d5730e96c2f0a6eb32f64937297338c8efb9
LeviVitez <bmwyolo555@gmail.com>
2023-03-16 12:56:36 +0100

    teszt

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
                 }
+                wait();
             }
         });
     }
